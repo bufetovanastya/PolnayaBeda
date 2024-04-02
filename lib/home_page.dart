17ae808c0f0c352f999ae7b013e9b1dd0200056d
Aramais07 <aramaismargaran8@gmail.com>
2024-04-02 10:34:22 +0300

    profile

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter_grid_view/cars_list.dart';
 import 'package:flutter_grid_view/favorites_builder.dart';
 import 'package:flutter_grid_view/history_builder.dart';
 import 'package:flutter_grid_view/models/cars.dart';
+import 'package:flutter_grid_view/profile_builder.dart';
+import 'profile_builder.dart';
 import 'cart_favorites.dart';
 
 class HomePage extends StatelessWidget {
@@ -36,8 +38,12 @@ class HomePage extends StatelessWidget {
           IconButton(onPressed: (){
             Navigator.push(context, MaterialPageRoute(builder: (context) => HistoryObject()));
           }, icon: Icon(Icons.history)
-          )
-        ],
+          ),
+          IconButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => ProfileObject()));
+          }, icon: Icon(Icons.person)
+    ),
+    ],
       ),
       body: GridView.builder(
           itemCount: carsList.length,
